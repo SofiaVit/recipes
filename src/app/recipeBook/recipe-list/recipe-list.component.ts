a15cd1ae3ab53d2f00093bfd5166ad6578c7cfb2
Sofiav <sofiav@mprest.com>
2021-06-13 12:18:17 +0300

    Little bit UI added

--- src/app/recipeBook/recipe-list/recipe-list.component.ts
+++ src/app/recipeBook/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Recipe} from "../../modules/recipe.model";
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+
+  recipes: Recipe[] = [
+    new Recipe('Bulochki', 'Buns with meat', '../../../assets/bulochki.jpeg')
+  ];
 
   constructor() { }
 
--- src/app/shoppingList/shopping-list/shopping-list.component.ts
+++ src/app/shoppingList/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Ingredient} from "../../modules/ingredient.model";
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
+
+  ingredients: Ingredient[] = [
+    new Ingredient('Ground beef', 500),
+    new Ingredient('Onions', 1)
+  ];
 
   constructor() { }
 
